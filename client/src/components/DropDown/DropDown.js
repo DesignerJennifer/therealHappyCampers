7d2777da009fcaa7574e17f951ed183bb8364a88
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-05-09 06:58:07 -0500

    updated registration form

---
+++ client/src/components/DropDown/DropDown.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        maxWidth: 300,
+    },
+    chips: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    chip: {
+        margin: 2,
+    },
+    noLabel: {
+        marginTop: theme.spacing(3),
+    },
+}));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+const sizes = [
+    'Youth Small',
+    'Youth Medium',
+    'Youth Large',
+    'Youth XLarge',
+    'Adult XSmall',
+    'Adult Small',
+    'Adult Medium',
+    'Adult Large',
+    'Adult XLarge',
+    'Adult XXLarge',
+    'Adult XXXLarge'
+];
+
+function getStyles(sizes, shirtSize, theme) {
+    return {
+        fontWeight:
+            shirtSize.indexOf(sizes) === -1
+                ? theme.typography.fontWeightRegular
+                : theme.typography.fontWeightMedium,
+    };
+}
+
+export default function MultipleSelect() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [sizes, setSizes] = React.useState([]);
+
+    const handleChange = (event) => {
+        setSizes(event.target.value);
+    };
+
+    const handleChangeMultiple = (event) => {
+        const { options } = event.target;
+        const value = [];
+        for (let i = 0, l = options.length; i < l; i += 1) {
+            if (options[i].selected) {
+                value.push(options[i].value);
+            }
+        }
+        setSizes(value);
+    };
+}
--- client/src/pages/Registration.js
+++ client/src/pages/Registration.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 
 import App from '../App';
 
@@ -21,9 +23,12 @@ export default function FormPropsTextFields() {
 
     return (
         <Container maxWidth="sm">
-
+            <div className={classes.root}>
+                <Typography variant="h5">Parent/Gaurdian Information</Typography>
+            </div>
             <form className={classes.root} noValidate autoComplete="off">
                 <div>
+                    {/* firstname - Gardian */}
                     <TextField
                         required id="standard-required"
                         label="Required"
@@ -32,6 +37,7 @@ export default function FormPropsTextFields() {
                 </div>
 
                 <div>
+                    {/* lastname - Garidan */}
                     <TextField
                         required id="standard-required"
                         label="Required"
@@ -40,6 +46,7 @@ export default function FormPropsTextFields() {
                 </div>
 
                 <div>
+                    {/* email */}
                     <TextField
                         required id="standard-required"
                         label="Required"
@@ -49,6 +56,7 @@ export default function FormPropsTextFields() {
                 </div>
 
                 <div>
+                    {/* phonenumber */}
                     <TextField
                         required id="standard-required"
                         label="Required"
@@ -56,13 +64,93 @@ export default function FormPropsTextFields() {
                     />
                 </div>
 
-                <TextField
-                    id="standard-helperText"
-                    label="Helper text"
-                    defaultValue="Default Value"
-                    helperText="Some important text"
-                />
+                <div className={classes.root}>
+                    <Typography variant="h5">Camper Information</Typography>
+                </div>
 
+                <div>
+                    {/* camperfirstname */}
+                    <TextField
+                        required id="standard-required"
+                        label="Required"
+                        defaultValue="First Name"
+                    />
+                </div>
+
+                <div>
+                    {/* camperlastname */}
+                    <TextField
+                        required id="standard-required"
+                        label="Required"
+                        defaultValue="Last Name"
+                    />
+                </div>
+                <div>
+                    {/* nickname */}
+                    <TextField
+                        id="standard-helperText"
+                        label="Required"
+                        defaultValue="Preferred Name"
+                        helperText="Name your camper would like to be called at camp"
+                    />
+                </div>
+
+                {/* birthday */}
+                <TextField
+                    id="date"
+                    label="Required"
+                    type="date"
+                    defaultValue="2017-05-24"
+                    className={classes.textField}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    helperText="Camper's birthdate"
+
+                />
+                <div>
+                    {/* grade */}
+                    <TextField
+                        id="standard-helperText"
+                        label="Required"
+                        defaultValue="4"
+                        helperText="Grade your camper will be entering in the fall"
+
+                    />
+                </div>
+
+                <div>
+                    {/* Shirtsize */}
+                    <TextField
+                        id="standard-helperText"
+                        label="Required"
+                        defaultValue="Medium"
+                        helperText="Camper's Tshirt Size. Youth and adult sizes available"
+
+                    />
+                </div>
+
+                <div>
+                    {/* allergies */}
+                    <TextField
+                        id="standard-helperText"
+                        label="Required"
+                        defaultValue="Allergies"
+                        helperText="Please list your Camper's known allergies"
+
+                    />
+                </div>
+
+                <div>
+                    {/* dietaryneeds */}
+                    <TextField
+                        id="standard-helperText"
+                        label="Required"
+                        defaultValue="No Prefrence"
+                        helperText="Please list your Camper's dietary needs (example: vegitarian)"
+
+                    />
+                </div>
 
             </form>
         </Container>
