441abd74d18baf2902bd574ba679d02fd4d509f3
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-05-14 18:45:24 -0500

    updated replaced pages neede - Removed 'forgot password...'

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -83,9 +83,9 @@ export default function SignIn() {
                     </Button>
 
                     <Grid container>
-                        <Grid item xs>
+                        {/* <Grid item xs>
                             <Link href="#" variant="body2">Forgot password?</Link>
-                        </Grid>
+                        </Grid> */}
                         <Grid item>
                             <Link href="CheckoutForm" variant="body2">"Don't have an account? Sign Up"</Link>
 
