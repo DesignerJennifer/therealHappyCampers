c1a419848e9b049be749328d9d9106d48933bed5
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-05-16 15:24:54 -0500

    title route

--- client/src/App.js
+++ client/src/App.js
@@ -25,22 +25,4 @@ function App() {
   );
 }
 
-// import React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Login from "./pages/Login";
-// import Registration from "./pages/Registration";
-
-// function App() {
-//   return (
-//     <Router>
-//       <div>
-
-//         <Route exact path="/" component={Login} />
-//         <Route exact path="/registration" component={Registration} />
-
-//       </div>
-//     </Router>
-//   );
-// }
-
 export default App;
