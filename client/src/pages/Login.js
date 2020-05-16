f42fb32062ceb5b882535ae02db81ac427b4f0ac
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-05-16 10:51:59 -0500

    changing path and organizing files

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -10,96 +10,103 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import CQKC_Logo_wBorder_250x250 from "../images/CQKC_Logo_wBorder_250x250.png"
-import Copyright from "../components/Copyright";
+import CQKC_Logo_wBorder_250x250 from '../images/CQKC_Logo_wBorder_250x250.png';
+import Copyright from '../components/Copyright';
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%',
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
-
-
 export default function SignIn() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <div className={classes.paper}>
-                <div>
-                    <img className="logo_wBorder" src={CQKC_Logo_wBorder_250x250} alt="CQKC Logo" />
-                </div>
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <div>
+          <img
+            className="logo_wBorder"
+            src={CQKC_Logo_wBorder_250x250}
+            alt="CQKC Logo"
+          />
+        </div>
 
-                <Typography component="h1" variant="h5">
-                    Sign in
-                 </Typography>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
 
-                <form className={classes.form} noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required fullWidth id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
 
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required fullWidth name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
 
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
 
-                    <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-                        Sign In
-                    </Button>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}>
+            Sign In
+          </Button>
 
-                    <Grid container>
-                        {/* <Grid item xs>
+          <Grid container>
+            {/* <Grid item xs>
                             <Link href="#" variant="body2">Forgot password?</Link>
                         </Grid> */}
-                        <Grid item>
-                            <Link href="CreateAccount" variant="body2">Don't have an account? Sign Up</Link>
+            <Grid item>
+              <Link href="CreateAccount" variant="body2">
+                Don't have an account? Sign Up
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
 
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-
-            <Box mt={8}>
-                <Copyright />
-            </Box>
-
-        </Container>
-
-
-    );
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
 }
-
-
