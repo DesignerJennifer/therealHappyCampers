4b590ad1623c92956db5675f6319f452996e2034
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-05-16 10:58:07 -0500

    made changes to sign up form

--- client/src/pages/SignUpForm.js
+++ client/src/pages/SignUpForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import registration from "../Utils/registration"
+import signup from "../Utils/signup.json"
 
 
 export default function SignUpForm() {
@@ -10,42 +10,73 @@ export default function SignUpForm() {
         <React.Fragment>
             <Typography variant="h6" gutterBottom>
                 Your Information
-      </Typography>
+            </Typography>
+
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6}>
+
+                    {/* Garudian First Name */}
                     <TextField
                         required
-                        id={registration.firstName}
+                        id={signup.firstName}
                         name="firstName"
                         label="First name"
                         fullWidth
                         autoComplete="fname"
                     />
                 </Grid>
+
+                {/* Gaurdian Last Name */}
                 <Grid item xs={12} sm={6}>
                     <TextField
                         required
-                        id={registration.lastName}
+                        id={signup.lastName}
                         name="lastName"
                         label="Last name"
                         fullWidth
                         autoComplete="lname"
                     />
                 </Grid>
+
+                {/* Username */}
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                        required
+                        id={signup.userName}
+                        name="lastName"
+                        label="Last name"
+                        fullWidth
+                        autoComplete="lname"
+                    />
+                </Grid>
+
+                {/* email */}
+                <TextField
+                    required
+                    id="email"
+                    name={signup.email}
+                    label="email"
+                    fullWidth
+                    autoComplete="email"
+                />
+
+
+                {/* phone number */}
                 <Grid item xs={12} sm={6}>
                     <TextField
                         required
                         id="phoneNumber"
-                        name={registration.phoneNumber}
+                        name={signup.phoneNumber}
                         label="Phone Number"
                         fullWidth
                         autoComplete="Phone Number"
                     />
                 </Grid>
+
                 <Grid item xs={12}>
                     <TextField
                         id="address"
-                        name={registration.address}
+                        name={signup.address}
                         label="Address"
                         fullWidth
                         autoComplete="Address"
@@ -55,7 +86,7 @@ export default function SignUpForm() {
                     <TextField
                         required
                         id="city"
-                        name={registration.city}
+                        name={signup.city}
                         label="City"
                         fullWidth
                         autoComplete="City"
@@ -68,7 +99,7 @@ export default function SignUpForm() {
                     <TextField
                         required
                         id="zip"
-                        name={registration.zip}
+                        name={signup.zip}
                         label="Zip / Postal code"
                         fullWidth
                         autoComplete="billing postal-code"
