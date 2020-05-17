8803204a351f0ea269643c991dce2ad023a48c41
Derrickas <Derrickas@outlook.com>
2020-05-16 20:12:00 -0500

    change to redirect

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,70 +17,70 @@ module.exports = function() {
     })
 
   }
-  //   db.User.update(data, where)
-  //     .then(function () {
-  //       res.redirect('/api/members')
-  //     })
-  //     .catch(function (err) {
-  //       console.error(JSON.stringify(err), data, where)
-  //       console.trace()
-  //       res.status(401).json(err)
-  //     })
-  // }
+    db.User.update(data, where)
+      .then(function () {
+        res.redirect('/api/members')
+      })
+      .catch(function (err) {
+        console.error(JSON.stringify(err), data, where)
+        console.trace()
+        res.status(401).json(err)
+      })
+  }
 
-  // app.get('/api/members', isAuthenticated, function (req, res) {
-  //   res.json({
-  //     email: req.user.email,
-  //     firstName: req.user.firstName,
-  //     lastName: req.user.lastName,
-  //     id: req.user.id
-  //   })
-  // })
+  app.get('/api/members', isAuthenticated, function (req, res) {
+    res.json({
+      email: req.user.email,
+      firstName: req.user.firstName,
+      lastName: req.user.lastName,
+      id: req.user.id
+    })
+  })
 
-  // app.post('/api/login', passport.authenticate('local'), function (req, res) {
-  //   // Sending back a password, even a hashed password, isn't a good idea
-  //   res.json({
-  //     email: req.user.email,
-  //     id: req.user.id
-  //   })
-  // })
+  app.post('/api/login', passport.authenticate('local'), function (req, res) {
+    // Sending back a password, even a hashed password, isn't a good idea
+    res.json({
+      email: req.user.email,
+      id: req.user.id
+    })
+  })
 
-  // // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // // otherwise send back an error
-  // app.post('/api/signup', function (req, res) {
-  //   db.User.create({
-  //     firstName: req.body.firstName,
-  //     lastName: req.body.lastName,
-  //     email: req.body.email,
-  //     password: req.body.password
-  //   })
-  //     .then(function () {
-  //       res.redirect(307, '/api/login')
-  //     })
-  //     .catch(function (err) {
-  //       res.status(401).json(err)
-  //     })
-  // })
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // otherwise send back an error
+  app.post('/api/signup', function (req, res) {
+    db.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(function () {
+        res.redirect(307, '/api/login')
+      })
+      .catch(function (err) {
+        res.status(401).json(err)
+      })
+  })
 
-  // // Route for logging user out
-  // app.get('/logout', function (req, res) {
-  //   req.logout()
-  //   res.redirect('/')
-  // })
+  // Route for logging user out
+  app.get('/logout', function (req, res) {
+    req.logout()
+    res.redirect('/')
+  })
 
-  // // Route for getting some data about our user to be used client side
-  // app.get('/api/user_data', isAuthenticated, function (req, res) {
-  //   db.User.findAll({
-  //     where: {
-  //       id: req.user.id
-  //     }
-  //   }).then(data => {
-  //     res.json(data[0])
-  //   })
-  // })
+  // Route for getting some data about our user to be used client side
+  app.get('/api/user_data', isAuthenticated, function (req, res) {
+    db.User.findAll({
+      where: {
+        id: req.user.id
+      }
+    }).then(data => {
+      res.json(data[0])
+    })
+  })
 
-  // // app.get('/api/signup', authController.signup);
+  // app.get('/api/signup', authController.signup);
 
   return router
 
