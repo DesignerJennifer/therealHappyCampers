bd48cb0c825b70e116eb3891ac724488939da7bf
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-05-23 10:10:10 -0500

    sequilize

--- models/index.js
+++ models/index.js
@@ -54,9 +54,14 @@ module.exports = db;
 
 module.exports = db;
 
-//  //   let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+//   let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
